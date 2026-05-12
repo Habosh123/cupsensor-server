2f4284ecd4c8ab76a77087779b4bd37f3a4faa21
Habosh123 <nkgolubnichenko1@gmail.com>
2026-05-12 21:48:08 +0500

    Use Vercel Web API function

--- api/fill.js
+++ api/fill.js
@@ -1,3 +1,14 @@
-export default async function handler(req, res) {
-  return res.status(200).send('CupSensor API works');
+export function GET() {
+  return new Response('CupSensor API works', {
+    status: 200
+  });
+}
+
+export async function POST(request) {
+  const body = await request.json();
+
+  return Response.json({
+    ok: true,
+    received: body
+  });
 }
